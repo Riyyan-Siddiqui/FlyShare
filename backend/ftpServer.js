7f51d022a355b38320286cd2f93dc92d9b089882
Riyyan Siddiqui <riyyan.s.24@gmail.com>
2025-05-04 18:27:55 +0500

    updated

--- backend/ftpServer.js
+++ backend/ftpServer.js
@@ -1,22 +1,22 @@
-const ftp = require("basic-ftp");
-require("dotenv").config();
+// const ftp = require("basic-ftp");
+// require("dotenv").config();
 
-async function uploadFile(localPath, remoteFileName) {
-  const client = new ftp.Client();
-  try {
-    await client.access({
-      host: process.env.FTP_HOST,
-      user: process.env.FTP_USER,
-      password: process.env.FTP_PASSWORD,
-      port: process.env.FTP_PORT || 21,
-      secure: false,
-    });
-    await client.uploadFrom(localPath, remoteFileName);
-    console.log("Uploaded:", remoteFileName);
-  } catch (err) {
-    console.error(err);
-  }
-  client.close();
-}
+// async function uploadFile(localPath, remoteFileName) {
+//   const client = new ftp.Client();
+//   try {
+//     await client.access({
+//       host: process.env.FTP_HOST,
+//       user: process.env.FTP_USER,
+//       password: process.env.FTP_PASSWORD,
+//       port: process.env.FTP_PORT || 21,
+//       secure: false,
+//     });
+//     await client.uploadFrom(localPath, remoteFileName);
+//     console.log("Uploaded:", remoteFileName);
+//   } catch (err) {
+//     console.error(err);
+//   }
+//   client.close();
+// }
 
-module.exports = { uploadFile };
+// module.exports = { uploadFile };
